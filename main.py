c7ffe621d0a09dc82bee116971d81638a0a4dc42
S1M0N38 <bertolottosimone@gmail.com>
2023-05-30 12:46:49 +0200

    Heavy refactor of the class Learner
    Now the class Learner is called Trainer and contains code for training
    and validation (the code for testing is tmp remove).
    
    The Trainer class use new TOML configuration file with new keys
    (e.g path_save to path).
    
    New two new features were introduced both based on tracking of loss_val
    and metrics_val:
    1. Save checkpoints according to best validation loss/metrics
    2. Early stopping triggered by patiences values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 import logging
 from hashlib import sha256
 from pathlib import Path
@@ -15,6 +16,210 @@ import models
 import losses
 
 
+class Trainer:
+    def __init__(self, experiemnt: str, config: dict):
+        self.config = copy.deepcopy(config)
+
+        # [dataloaders]
+        self.dataloader_train = init(dataloaders, config["dataloaders"]["train"])
+        self.dataloader_val = init(dataloaders, config["dataloaders"]["val"])
+
+        self.epochs, self.epoch = config.get("epochs", 0), 0
+        self.steps, self.step = self.epochs * len(self.dataloader_train), 0
+        self.period_val = config.get("validate", self.epochs)
+        self.period_log = config.get("log", self.steps)
+
+        self.gpus = list(range(config.get("num_gpus", 0)))
+        self.device = torch.device("cuda:0" if self.gpus else "cpu")
+
+        # [patience]
+        self.patience = config.get("patience", {})
+
+        # [model]
+        self.model = init(models, config["model"]).to(self.device)
+        if len(self.gpus) > 1:
+            self.model = torch.nn.DataParallel(self.model, device_ids=self.gpus)
+
+        # [loss]
+        self.loss = init(losses, config["loss"])
+        self.loss_train = torchmetrics.MeanMetric().to(self.device)
+        self.loss_val = torchmetrics.MetricTracker(
+            torchmetrics.MeanMetric().to(self.device),
+            maximize=False,
+        )
+
+        # [optimizer]
+        config["optimizer"]["params"] = self.model.parameters()
+        self.optimizer = init(torch.optim, config["optimizer"])
+
+        # [lr_scheduler]
+        config["lr_scheduler"]["optimizer"] = self.optimizer
+        self.lr_scheduler = init(torch.optim.lr_scheduler, config["lr_scheduler"])
+
+        # [metrics]
+        self.metrics_train = torchmetrics.MetricCollection(
+            {
+                name: init(metrics, metric).to(self.device)
+                for name, metric in config["metrics"]["train"].items()
+            }
+        )
+        self.metrics_val = torchmetrics.MetricTracker(
+            torchmetrics.MetricCollection(
+                {
+                    name: init(metrics, metric).to(self.device)
+                    for name, metric in config["metrics"]["val"].items()
+                }
+            )
+        )
+
+        # Track and save results
+        self.path = Path(config.get("path", ".")) / experiemnt
+        self.writer = SummaryWriter(self.path / "runs")
+        self.logger = init_logger(self.path / "trainer.log")
+        (self.path / "checkpoints").mkdir()
+        with open(self.path / "trainer.toml", "w") as f:
+            toml.dump(self.config, f)
+
+    def __str__(self):
+        return toml.dumps(self.config)
+
+    def _train(self):
+        self.model = self.model.train()
+        with torch.enable_grad():
+            for inputs, targets in self.dataloader_train:
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                outputs = self.model(inputs)
+                loss = self.loss(outputs, targets)
+                loss.backward()
+                self.metrics_train(outputs, targets)
+                self.loss_train(loss)
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                self.step += 1
+
+                if self.step % self.period_log == 0:
+                    metrics = self.metrics_train.compute()
+                    metrics["loss"] = self.loss_train.compute()
+                    self._log("debug", "train", metrics)
+                    self.metrics_train.reset()
+                    self.loss_train.reset()
+
+                if self.step == self.steps:
+                    break
+
+            self.epoch += 1
+            self._log("info", "train")
+            self.lr_scheduler.step()
+
+    def _val(self):
+        self.model = self.model.eval()
+        self.metrics_val.increment()
+        self.loss_val.increment()
+        with torch.no_grad():
+            for inputs, targets in self.dataloader_val:
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                outputs = self.model(inputs)
+                loss = self.loss(outputs, targets)
+                self.metrics_val(outputs, targets)
+                self.loss_val(loss)
+
+            metrics = self.metrics_val.compute()
+            metrics["loss"] = self.loss_val.compute()
+            self._log("info", "val", metrics)
+
+    def _eta(self) -> str:
+        enlapsed = datetime.now() - self.start_time
+        eta = (enlapsed / self.step * (self.steps - self.step)).seconds
+        return f"{eta // 3600:>2}:{(eta % 3600) // 60:02}:{eta % 60:02}"
+
+    def _log(self, level: str = "info", mode: str = "train", metrics: dict = {}):
+        message = (
+            f"{mode.capitalize():<5} [{(self.step / self.steps) * 100 :3.0f}%] | "
+            f"Epoch [{self.epoch}/{self.epochs}] | "
+            f"Step [{self.step}/{self.steps}] | "
+            f"ETA {self._eta()} | "
+            f"{', '.join([f'{n} {m.item():.3f}' for n, m in metrics.items()])}"
+        )
+        getattr(self.logger, level)(message)
+
+        for name, value in metrics.items():
+            self.writer.add_scalar(f"{mode}/{name}", value, self.step)
+
+    def _save(self, name: str):
+        syml = self.path / "checkpoints" / name
+        syml.unlink(missing_ok=True)  # remove old symlink
+        path = self.path / "checkpoints" / f"{self.step}.pt"
+        if not path.exists():
+            checkpoint = {
+                "config": self.config,
+                "model": self.model.state_dict(),
+                "optimizer": self.optimizer.state_dict(),
+                "lr_scheduler": self.lr_scheduler.state_dict(),
+            }
+            torch.save(checkpoint, path)
+        syml.symlink_to(f"{self.step}.pt")
+
+    def _clean(self):
+        path = self.path / "checkpoints"
+        active = {p.resolve() for p in path.glob("*.pt") if p.is_symlink()}
+        [p.unlink() for p in path.glob("*.pt") if p.resolve() not in active]
+
+    def _load(self):
+        raise NotImplementedError("This method is not implemented yet.")
+
+    def train(self):
+        self.start_time = datetime.now()
+
+        while True:
+            self._train()
+
+            if self.epoch % self.period_val == 0:
+                self._val()
+
+                # Find the index (from the end) of the best step for loss_val
+                _, loss_best_step = self.loss_val.best_metric(return_step=True)
+                loss_best_k_last = self.loss_val.n_steps - loss_best_step
+
+                # Find the index (from the end) of the bests steps for metrics_val
+                _, metrics_best_steps = self.metrics_val.best_metric(return_step=True)
+                metrics_best_k_last = {
+                    metric: self.metrics_val.n_steps - metric_best_step
+                    for metric, metric_best_step in metrics_best_steps.items()
+                }
+
+                # Save checkpoint with best loss_val
+                if loss_best_k_last == 1:
+                    self._save("loss_val.pt")
+
+                # Save checkpoints with best metrics_val
+                for metric, metric_best_k_last in metrics_best_k_last.items():
+                    if metric_best_k_last == 1:
+                        self._save(f"{metric}.pt")
+
+                # Remove old checkpoints that have no symlink that points to them
+                self._clean()
+
+                # Trigger early stopping for loss_val
+                if loss_best_k_last - 1 > self.patience.get("loss", self.epochs):
+                    self.logger.info("Early stopping trigger by 'loss'.")
+                    break
+
+                # Trigger early stopping for metrics_val
+                for metric, metric_best_k_last in metrics_best_k_last.items():
+                    if metric_best_k_last - 1 > self.patience.get(metric, self.epochs):
+                        self.logger.info(f"Early stopping trigger by '{metric}'.")
+                        break
+                else:
+                    continue
+                break
+
+            if self.step == self.steps:
+                break
+
+        # Load last checkpoint
+        self._save("last.pt")
+
+
 def init(module: object, name: dict):
     class_name = name["class"]
     del name["class"]
@@ -22,241 +227,30 @@ def init(module: object, name: dict):
     return getattr(module, class_name)(**class_args)
 
 
-def logger(path: Path):
+def init_logger(path: Path):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     fmt = logging.Formatter("%(levelname)8s - %(asctime)s - %(message)s")
     fh = logging.FileHandler(path)
-    sh = logging.StreamHandler()
-    fh.setLevel(logging.INFO)
-    sh.setLevel(logging.DEBUG)
+    fh.setLevel(logging.DEBUG)
     fh.setFormatter(fmt)
-    sh.setFormatter(fmt)
     logger.addHandler(fh)
-    logger.addHandler(sh)
     return logger
 
 
-class Learner:
-    def __init__(self, config: Path):
-        now = datetime.now()
-        self.config = toml.load(config)
-        hash = sha256(toml.dumps(self.config).encode()).hexdigest()[:8]
-        self.name = f"{now.strftime('%m%d_%H%M')}_{hash}"
-
-        # [dataloaders]
-        self.train_dataloader = init(dataloaders, self.config["dataloaders"]["train"])
-        self.val_dataloader = init(dataloaders, self.config["dataloaders"]["val"])
-        self.test_dataloader = init(dataloaders, self.config["dataloaders"]["test"])
-        self.batches = len(self.train_dataloader)
-
-        # [learner]
-        self.epochs, self.epoch = self.config["learner"]["epochs"], 0
-        self.steps, self.step = self.epochs * self.batches, 0
-        self.val_period = self.config["learner"].get("val_period", self.epochs)
-        self.save_period = self.config["learner"].get("save_period", self.epochs)
-        self.log_period = self.config["learner"].get("log_period", self.steps)
-        self.save_path = Path(self.config["learner"]["save_path"]) / self.name
-        self.save_path.mkdir(parents=True, exist_ok=True)
-        self.load_path = self.config["learner"].get("load_path")
-        self.writer = SummaryWriter(self.save_path / "runs")
-
-        # [device]
-        self.gpus = list(range(self.config["device"]["num_gpus"]))
-        self.device = torch.device("cuda:0" if self.gpus else "cpu")
-
-        # [model]
-        self.model = init(models, self.config["model"]).to(self.device)
-        if len(self.gpus) > 1:
-            self.model = torch.nn.DataParallel(self.model, device_ids=self.gpus)
-
-        # [loss]
-        self.loss = init(losses, self.config["loss"])
-        self.train_loss = torchmetrics.MeanMetric().to(self.device)
-        self.val_loss = torchmetrics.MeanMetric().to(self.device)
-        self.test_loss = torchmetrics.MeanMetric().to(self.device)
-
-        # [optimizer]
-        self.config["optimizer"]["params"] = self.model.parameters()
-        self.optimizer = init(torch.optim, self.config["optimizer"])
-
-        # [lr_scheduler]
-        self.config["lr_scheduler"]["optimizer"] = self.optimizer
-        self.lr_scheduler = init(torch.optim.lr_scheduler, self.config["lr_scheduler"])
-
-        # [metrics]
-        self.train_metrics = torchmetrics.MetricCollection(
-            {
-                name: init(metrics, metric).to(self.device)
-                for name, metric in self.config["metrics"]["train"].items()
-            }
-        )
-        self.val_metrics = torchmetrics.MetricCollection(
-            {
-                name: init(metrics, metric).to(self.device)
-                for name, metric in self.config["metrics"]["val"].items()
-            }
-        )
-        self.test_metrics = torchmetrics.MetricCollection(
-            {
-                name: init(metrics, metric).to(self.device)
-                for name, metric in self.config["metrics"]["test"].items()
-            }
-        )
-
-        # Hyperparameters for logging
-        self.hparams = {
-            "model": self.model.__class__.__name__,
-            "loss": self.loss.__class__.__name__,
-            "optimizer": self.optimizer.__class__.__name__,
-            "lr_scheduler": self.lr_scheduler.__class__.__name__,
-            "init_lr": self.optimizer.param_groups[0]["lr"],
-            "batch_size": self.train_dataloader.batch_size,
-            "epochs": self.epochs,
-            "steps": self.steps,
-        }
-
-        # Init logger
-        self.logger = logger(self.save_path / "learner.log")
-
-        # Save config
-        with config.open("rb") as src_file:
-            with (self.save_path / "config.toml").open("wb") as dst_file:
-                dst_file.write(src_file.read())
-
-        # Load model and update steps
-        if self.load_path:
-            self.load_path = Path(self.load_path)
-            self.step = int(self.load_path.stem)
-            self.steps += int(self.load_path.stem)
-            self._load(self.load_path)
-
-    def _train(self) -> bool:
-        self.model = self.model.train()
-        with torch.enable_grad():
-            for inputs, targets in self.train_dataloader:
-                if self.step == self.steps:
-                    return True
-                inputs, targets = inputs.to(self.device), targets.to(self.device)
-                outputs = self.model(inputs)
-                loss = self.loss(outputs, targets)
-                loss.backward()
-                self.train_metrics(outputs, targets)
-                self.train_loss(loss)
-                self.optimizer.step()
-                self.optimizer.zero_grad()
-                self.step += 1
-
-                if self.step % self.log_period == 0:
-                    metrics = self.train_metrics.compute()
-                    metrics["loss"] = self.train_loss.compute()
-                    self._log("debug", "train", metrics, add_scalar=True)
-                    self.train_metrics.reset()
-                    self.train_loss.reset()
-
-            self.epoch += 1
-            self._log("info", "train")
-            self.lr_scheduler.step()
-        return False
-
-    def _val(self):
-        self.model = self.model.eval()
-        with torch.no_grad():
-            for inputs, targets in self.val_dataloader:
-                inputs, targets = inputs.to(self.device), targets.to(self.device)
-                outputs = self.model(inputs)
-                loss = self.loss(outputs, targets)
-                self.val_metrics(outputs, targets)
-                self.val_loss(loss)
-
-            metrics = self.val_metrics.compute()
-            metrics["loss"] = self.val_loss.compute()
-            self._log("info", "val", metrics, add_scalar=True)
-            self.val_metrics.reset()
-            self.val_loss.reset()
-
-    def _test(self):
-        self.model = self.model.eval()
-        with torch.no_grad():
-            for inputs, targets in self.test_dataloader:
-                inputs, targets = inputs.to(self.device), targets.to(self.device)
-                outputs = self.model(inputs)
-                loss = self.loss(outputs, targets)
-                self.test_metrics(outputs, targets)
-                self.test_loss(loss)
-
-            metrics = self.test_metrics.compute()
-            metrics["loss"] = self.test_loss.compute()
-            self._log("info", "test", metrics, add_hparams=True)
-            self.test_metrics.reset()
-            self.test_loss.reset()
-
-    def _save(self, path: Path):
-        ckpt = {
-            "model": self.model.state_dict(),
-            "optimizer": self.optimizer.state_dict(),
-            "lr_scheduler": self.lr_scheduler.state_dict(),
-        }
-        path.parent.mkdir(parents=True, exist_ok=True)
-        torch.save(ckpt, path)
-        self.logger.info(f"Save  {path.relative_to('.')}")
-
-    def _load(self, path: Path):
-        ckpt = torch.load(path)
-        self.model.load_state_dict(ckpt["model"])
-        self.optimizer.load_state_dict(ckpt["optimizer"])
-        self.lr_scheduler.load_state_dict(ckpt["lr_scheduler"])
-        self.logger.info(f"Load  {path.relative_to('.')}")
-
-    def _log(
-        self,
-        level: str = "info",
-        mode: str = "train",
-        metrics: dict = {},
-        add_scalar: bool = False,
-        add_hparams: bool = False,
-    ):
-        getattr(self.logger, level)(
-            (
-                f"{mode.capitalize():<5} [{(self.step / self.steps) * 100 :3.0f}%] | "
-                f"Epoch [{self.epoch}/{self.epochs}] | "
-                f"Step [{self.step}/{self.steps}] | "
-                f"ETA {self._eta()} | "
-                f"{', '.join([f'{n} {m.item():.3f}' for n, m in metrics.items()])}"
-            )
-        )
-
-        if add_scalar:
-            for name, value in metrics.items():
-                self.writer.add_scalar(f"{mode}/{name}", value, self.step)
-
-        if add_hparams:
-            metrics = {f"{mode}/{k}": v.item() for k, v in metrics.items()}
-            self.writer.add_hparams(self.hparams, metrics, run_name="../runs")
-
-    def _eta(self):
-        enlapsed = datetime.now() - self.start_time
-        eta = (enlapsed / self.step * (self.steps - self.step)).seconds
-        return f"{eta // 3600:>2}:{(eta % 3600) // 60:02}:{eta % 60:02}"
-
-    def train(self):
-        self.start_time = datetime.now()
-        while True:
-            finished = self._train()
-            if self.epoch % self.val_period == 0 or self.step == self.steps:
-                self._val()
-            if self.epoch % self.save_period == 0 or self.step == self.steps:
-                self._save(self.save_path / "checkpoints" / f"{self.step}.pt")
-            if finished or self.step == self.steps:
-                break
-        self.load_path = self.save_path / "checkpoints" / f"{self.step}.pt"
-
-    def test(self):
-        self._test()
+def generate_experiment_name(config: dict):
+    now = datetime.now()
+    hash = sha256(toml.dumps(config).encode()).hexdigest()[:8]
+    return f"{now.strftime('%m%d_%H%M')}_{hash}"
 
 
 if __name__ == "__main__":
-    config = Path(sys.argv[1])
-    learner = Learner(config)
-    learner.train()
-    learner.test()
+    config = toml.load(Path(sys.argv[1]))
+    experiemnt = generate_experiment_name(config)
+    trainer = Trainer(experiemnt, config)
+
+    print(trainer)
+    print(f"Progress at {trainer.path / 'trainer.log'}")
+    print("Training ...")
+
+    trainer.train()
